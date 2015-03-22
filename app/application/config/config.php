60c455bf5216e503cea8b33bb4c31bba1dc7ef6a
Lewis Nelson <lewis@lewnelson.com>
2015-03-22 12:30:34 +0000

    Update config.php

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'Iq8Q4CqRa9JNHJ2hdiwEpvd8xkx8zzZU';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
