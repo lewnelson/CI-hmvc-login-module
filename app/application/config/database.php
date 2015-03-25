9a0cfbb0048686a144ae2e04ca52615e33d3cffc
lewnelson <lewis@lewnelson.com>
2015-03-25 15:27:40 +0000

    Creating example configurations

--- app/application/config/database.php
+++ app/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'test';
-$db['default']['password'] = 'test';
-$db['default']['database'] = 'test';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
